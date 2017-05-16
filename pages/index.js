fc37baef02135e8281d16dd8c2a9e67689b0dec9
Mario Colque <colquemario@gmail.com>
2017-05-15 21:10:05 -0300

    added redirect to /deployments

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 // Packages
+import cookies from 'next-cookies'
 import React from 'react'
+import Router from 'next/router'
 
 // Ours
 import Layout from '../components/Layout'
 
-export default () => (
-  <div>
-    <Layout/>
+class Index extends React.Component {
+  getInitialProps(ctx) {
+    const {token} = cookies(ctx)
 
-    <div id="home">
-      <div className="container">
-        <h1 className="title">▲ admin</h1>
+    if (token) {
+      Router.push('/deployments')
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout/>
+
+        <div id="home">
+          <div className="container">
+            <h1 className="title">▲ admin</h1>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
+
+export default Index
